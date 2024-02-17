31cf3aebf36341461f2aa779a5d32626e9b27f5e
Mateusz Kamyk <157246319+mateusz-kamyk@users.noreply.github.com>
2024-02-17 22:04:14 +0100

    Added input_data checking

--- ExchangeRateDifferences/Calculator/main.py
+++ ExchangeRateDifferences/Calculator/main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import input_data
+import input_data_check
 import api_requests
 import calculator
 
 def main():
     #input data (regarding the invoice and user payment), collect information
-    invoice_amount, invoice_currency, invoice_date, user_amount, user_currency, user_date = input_data.run_input_data()
+    invoice_amount, invoice_currency, invoice_date, user_amount, user_currency, user_date = input_data.input_data()
+    input_data_check.check_data(invoice_amount, invoice_currency, invoice_date, user_amount, user_currency, user_date)
 
     #Collect mid values (exchange rate values) for invoice and user data.
     invoice_mid_value = api_requests.request_data(invoice_currency, invoice_date)
