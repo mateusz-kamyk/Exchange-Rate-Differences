4c163fedc489a703d6fe912cb7c0c0a048b405f1
Mateusz Kamyk <157246319+mateusz-kamyk@users.noreply.github.com>
2024-02-18 11:23:47 +0100

    Fixed logical issues with the result calculation

--- ExchangeRateDifferences/Calculator/calculator.py
+++ ExchangeRateDifferences/Calculator/calculator.py
@@ -5,12 +5,12 @@ def calculate_difference(invoice_amount, user_amount, invoice_mid_value, user_mi
     #Multiplying the amount with the exchange rate
     total_invoice_amount = float(invoice_amount) * float(invoice_mid_value)
     total_user_amount = float(user_amount) * float(user_mid_value)
-    difference = total_invoice_amount - total_user_amount     #Difference in PLN
+    difference = total_user_amount - total_invoice_amount     #Difference in PLN
     
     if difference <= -0.01:
-        print(f"You will receive a refund: {'{:.2f}'.format(abs(difference))} PLN")
+        print(f"An additional amount must be paid: {'{:.2f}'.format(abs(difference))} PLN")
     elif difference >= 0.01:
-        print(f"An additional amount must be paid: {'{:.2f}'.format(difference)} PLN")
+        print(f"You will receive a refund: {'{:.2f}'.format(difference)} PLN")
     else:
         print("Paid the full amount of the invoice.")
 
