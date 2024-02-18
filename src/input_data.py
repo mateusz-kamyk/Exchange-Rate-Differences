e5ca91984292e3fad309f30194597eeea5b8d6b2
Mateusz Kamyk <157246319+mateusz-kamyk@users.noreply.github.com>
2024-02-18 21:11:39 +0100

    Removed double brackets - cosmetic fix

--- src/input_data.py
+++ src/input_data.py
@@ -5,7 +5,7 @@ def input_data():
     print("Exchange Rate Differences program")
     print("Please provide the invoice data:")
     invoice_amount = input("The amount to be paid:\n")
-    invoice_currency = input("The currency ((PLN, EUR, USD, or GBP)):\n")
+    invoice_currency = input("The currency (PLN, EUR, USD, or GBP):\n")
     invoice_date = input("The invoice date (YYYY-MM-DD):\n")
     print("Please provide your payment data:")
     user_amount = input("Paid amount:\n")
