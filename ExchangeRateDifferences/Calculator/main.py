f11936d2a790c86b8c585513b9d9f302b5930c69
Mateusz Kamyk <157246319+mateusz-kamyk@users.noreply.github.com>
2024-02-18 02:49:26 +0100

    Added the option to save a file with possibility to view the --help

--- ExchangeRateDifferences/Calculator/main.py
+++ ExchangeRateDifferences/Calculator/main.py
@@ -17,5 +17,28 @@ def main():
     #Calculate if a refund or additional payment is required
     calculator.calculate_difference(invoice_amount, user_amount, invoice_mid_value, user_mid_value)
 
+    return invoice_amount, invoice_currency, invoice_date, user_amount, user_currency, user_date, invoice_mid_value, user_mid_value, difference
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-f', '--filename', help='It is possible to enter a file name in order to save a summary of the calculation.\nAvailable formats: .txt, .csv.')
+    parser.epilog = """
+    Example of use:
+
+    ./main.py -f invoice-status-summary.txt
+    ./main.py --filename invoice-status-summary.txt
+    
+    If you do not need a file with summary:
+    ./main.py
+
+    """
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    main()    
+    args = parse_arguments()
+    if args.filename:
+        invoice_amount, invoice_currency, invoice_date, user_amount, user_currency, user_date, invoice_mid_value, user_mid_value, difference = main()
+        save_file.save_to_file(invoice_amount, invoice_currency, invoice_date, user_amount, user_currency, user_date, invoice_mid_value, user_mid_value, difference, args.filename)
+    else:
+        main()
+        
